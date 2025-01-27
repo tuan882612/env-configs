1c8eda3252d07da62f3867ce4c07512023fc11ef
Tuan Trinh <tuan882612@gmail.com>
2025-01-27 17:13:08 -0600

    Updated nvim key mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,7 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "kj", "<ESC>")
 
 map("n", "<leader>cf", function()
   require("conform").format { async = true }
