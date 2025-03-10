238fd788843730d14f420ef217deaf7f7f5a3634
Tuan Trinh <tuan882612@gmail.com>
2025-03-09 20:18:49 -0500

    Update Neovim plugin versions and enhance visual line mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -13,3 +13,9 @@ map("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename variable using LSP" }
 map("n", "<leader>fr", function()
   vim.lsp.buf.references()
 end, { desc = "Telescope find references" })
+
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" })
+
+map("v", "<Tab>", ">gv", { desc = "Indent selected text" })
+map("v", "<S-Tab>", "<gv", { desc = "Unindent selected text" })
