7c877f344ffb72ebedf672e5b2ba02664f8bc15a
Tuan Trinh <tuan882612@gmail.com>
2025-09-29 08:10:01 -0500

    Revert lspconfig settings

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,39 +1,34 @@
 require("nvchad.configs.lspconfig").defaults()
 
--- You can still use lspconfig.util for root detection (data-only)
+local lspconfig = require "lspconfig"
 local util = require("lspconfig.util")
 
--- Shared opts
-local on_attach = on_attach
-local capabilities = capabilities
-
--- gopls
-vim.lsp.config('gopls', {
+lspconfig.gopls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  filetypes = {
+    "go", "gomod", "gowork", "gotmpl",
+  },
   root_dir = util.root_pattern("go.work", "go.mod", ".git"),
   settings = {
     gopls = {
       completeUnimported = true,
-      analyses = { unusedparams = true },
+      analyses = {
+        unusedparams = true,
+      },
     },
   },
-})
-vim.lsp.enable('gopls')
+}
 
--- ruff
-vim.lsp.config('ruff', {
+lspconfig.ruff.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "python" },
   root_dir = util.root_pattern("pyproject.toml", ".git"),
-})
-vim.lsp.enable('ruff')
+}
 
--- pyright
-vim.lsp.config('pyright', {
+lspconfig.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   root_dir = util.root_pattern("pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", ".git"),
@@ -46,42 +41,40 @@ vim.lsp.config('pyright', {
         autoImportCompletions = true,
         diagnosticMode = "workspace",
       },
-      pythonPath = vim.fn.exepath("python"),
+      pythonPath = vim.fn.exepath("python")
     },
   },
-})
-vim.lsp.enable('pyright')
+}
 
--- zls
-vim.lsp.config('zls', {
+lspconfig.zls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "zig" },
   cmd = { "zls" },
-  root_dir = util.root_pattern("build.zig", ".git"),
-})
-vim.lsp.enable('zls')
+  root_dir = lspconfig.util.root_pattern("build.zig", ".git"),
+}
 
--- clangd
-vim.lsp.config('clangd', {
+lspconfig.clangd.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   cmd = { "clangd" },
   filetypes = { "c", "cpp", "objc", "objcpp", "h", "hpp" },
-  root_dir = util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
-})
-vim.lsp.enable('clangd')
+  root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
+}
 
--- TypeScript: tsserver was renamed to ts_ls in lspconfig
-vim.lsp.config('ts_ls', {
+lspconfig.ts_ls.setup {
   on_attach = function(client, bufnr)
-    -- prevent LSP formatting to avoid conflicts with prettier/etc.
-    client.server_capabilities.documentFormattingProvider = false
-    if type(on_attach) == "function" then on_attach(client, bufnr) end
+    if client.name == "tsserver" then
+      client.server_capabilities.documentFormattingProvider = false
+    end
   end,
   capabilities = capabilities,
   root_dir = util.root_pattern("package.json", "tsconfig.json", ".git"),
-  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+  },
   cmd = { "typescript-language-server", "--stdio" },
-})
-vim.lsp.enable('ts_ls')
+}
